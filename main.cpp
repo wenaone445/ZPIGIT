644827fd7c1aa67d6200d7995972e8beb5064c2f
[wenaone445] <[michal.szalast1@gmail.com]>
2020-03-28 13:12:09 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include<time.h>
+#include<stdio.h>
+using namespace std;
+
+int main() {
+int liczba;
+int wybor;
+int ktory_wybor = 0;
+    srand(time(NULL));
+        cout<<"maszyna losujaca wylosuje liczbe z przedzialu 1-1000"<<endl;
+        liczba = rand()%1000+1;
+
+            while(wybor!=liczba){
+            ktory_wybor++;
+            cout<<"zgadnij jaka: ";
+            cin>>wybor;
+            cout<<"to twoj "<<ktory_wybor<<" wybor"<<endl;
+            if  (wybor==liczba)cout<<"to ta liczba, udalo Ci sie zgadnac za"<<ktory_wybor<<" razem"<<endl;
+            if  (wybor>liczba)cout<<"podana przez ciebie liczba jest za duza, probuj dalej"<<endl;
+            else if  (wybor<liczba)cout<<" to za malo"<<endl;
+
+            }
+            system("pause");
+	return 0;
+}
